fad0b4cd595f0125fa94a63efdb1a0642e8a033b
Bayan2019 <saparbayevabt@gmail.com>
2026-04-26 10:50:12 +0500

    up until repositories

---
+++ internal/domain/user.go
@@ -0,0 +1,102 @@
+package domain
+
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+var (
+	ErrUserNotFound      = errors.New("user not found")
+	ErrInvalidUserID     = errors.New("invalid user id")
+	ErrInvalidUserInput  = errors.New("invalid user input")
+	ErrEmailAlreadyTaken = errors.New("email already exists")
+)
+
+type User struct {
+	ID           int64      `db:"id" json:"id"`
+	Email        string     `db:"email" json:"email"`
+	PasswordHash string     `db:"password_hash" json:"-"`
+	FirstName    string     `db:"first_name" json:"first_name"`
+	LastName     string     `db:"last_name" json:"last_name"`
+	IsActive     bool       `db:"is_active" json:"is_active"`
+	CreatedAt    time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt    time.Time  `db:"updated_at" json:"updated_at"`
+	DeletedAt    *time.Time `db:"deleted_at" json:"deleted_at,omitempty"`
+}
+
+type CreateUserInput struct {
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	IsActive     *bool  `json:"is_active,omitempty"`
+}
+
+type UpdateUserInput struct {
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	IsActive     *bool  `json:"is_active,omitempty"`
+}
+
+type ListUsersFilter struct {
+	Limit          int    `json:"limit"`
+	Offset         int    `json:"offset"`
+	Query          string `json:"query"`
+	IncludeDeleted bool   `json:"include_deleted"`
+}
+
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+////// accommodating functions
+
+func (in *CreateUserInput) NormalizeAndValidate() error {
+	in.Email = strings.TrimSpace(strings.ToLower(in.Email))
+	in.PasswordHash = strings.TrimSpace(in.PasswordHash)
+	in.FirstName = strings.TrimSpace(in.FirstName)
+	in.LastName = strings.TrimSpace(in.LastName)
+
+	if in.Email == "" || !strings.Contains(in.Email, "@") {
+		return ErrInvalidUserInput
+	}
+	if in.PasswordHash == "" || in.FirstName == "" || in.LastName == "" {
+		return ErrInvalidUserInput
+	}
+	return nil
+}
+
+func (in *UpdateUserInput) NormalizeAndValidate() error {
+	in.Email = strings.TrimSpace(strings.ToLower(in.Email))
+	in.PasswordHash = strings.TrimSpace(in.PasswordHash)
+	in.FirstName = strings.TrimSpace(in.FirstName)
+	in.LastName = strings.TrimSpace(in.LastName)
+
+	if in.Email == "" || !strings.Contains(in.Email, "@") {
+		return ErrInvalidUserInput
+	}
+	if in.PasswordHash == "" || in.FirstName == "" || in.LastName == "" {
+		return ErrInvalidUserInput
+	}
+	return nil
+}
+
+func (f *ListUsersFilter) Normalize() {
+	if f.Limit <= 0 || f.Limit > 100 {
+		f.Limit = 20
+	}
+	if f.Offset < 0 {
+		f.Offset = 0
+	}
+	f.Query = strings.TrimSpace(
+		strings.ToLower(f.Query))
+}
---
+++ internal/repository/postgres/db.go
@@ -0,0 +1,28 @@
+package postgres
+
+import (
+	"time"
+
+	"github.com/Bayan2019/rbk-it-school-hw-3/internal/config"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+func NewDB(cfg config.DatabaseConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Open("pgx", cfg.DSN())
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(30 * time.Minute)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
